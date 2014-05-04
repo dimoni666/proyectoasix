bdee07a465a49ee3f91c771b49492b927dbb81c5
dimoni666 <dimoni666@gmail.com>
2014-05-04 16:50:40 +0200

    usuario

--- formulario.php
+++ formulario.php
@@ -5,7 +5,7 @@ $apellidos = $_POST["apellidos"];
 $email = $_POST["email"];
 $consulta = $_POST["consulta"];
 
-$conect = mysql_connect("localhost","userasix","4s1xP4ssWd");
+$conect = mysql_connect("localhost","asixuser","4s1xP4ssWd");
 mysql_select_db("asixdb",$conect) OR die ("no se ha podido conectar");
 
 $insert = "INSERT INTO consultas VALUE ($nombre,$apellidos,$email,$consulta);";

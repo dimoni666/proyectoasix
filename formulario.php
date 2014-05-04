a12e76440c328777282b2d612abaabe010e94690
dimoni666 <dimoni666@gmail.com>
2014-05-04 17:20:00 +0200

    comillas php

--- formulario.php
+++ formulario.php
@@ -8,7 +8,7 @@ $consulta = $_POST["motivo"];
 $conect = mysql_connect("localhost","asixuser","4s1xP4ssWd");
 mysql_select_db("asixdb",$conect) OR die ("no se ha podido conectar");
 
-$insert = "INSERT INTO consultas VALUE ($nombre,$apellidos,$email,$consulta);";
+$insert = "INSERT INTO consultas VALUE ('$nombre','$apellidos','$email','$consulta');";
 
 mysql_query($insert,$conect);
 

86c0d36f0389b30fa1db81cdcac448736b7ccbce
dimoni666 <dimoni666@gmail.com>
2014-05-04 17:14:54 +0200

    textarea y nombres

--- formulario.php
+++ formulario.php
@@ -3,12 +3,13 @@
 $nombre = $_POST["nombre"];
 $apellidos = $_POST["apellidos"];
 $email = $_POST["email"];
-$consulta = $_POST["consulta"];
+$consulta = $_POST["motivo"];
 
 $conect = mysql_connect("localhost","asixuser","4s1xP4ssWd");
 mysql_select_db("asixdb",$conect) OR die ("no se ha podido conectar");
 
 $insert = "INSERT INTO consultas VALUE ($nombre,$apellidos,$email,$consulta);";
+
 mysql_query($insert,$conect);
 
 
